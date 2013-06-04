a5abd3a3da804ba9ab43ced0200f04d903bf7b18
nguyen trong long <traitim9x@gmail.com>
2013-06-04 10:46:30 +0700

    form dang ky nhan con nuoi

--- wp-content/themes/orphans-fd-theme/templates/page-becomeparents.php
+++ wp-content/themes/orphans-fd-theme/templates/page-becomeparents.php
@@ -21,7 +21,7 @@ get_header();
 							  <label for="txt-name" class="inline">Họ tên</label>
 							</div>
 							<div class="small-9 columns">
-							  <input type="text" id="txt-name" placeholder="Họ tên" />
+							  <input type="text" name="txt-name" id="txt-name" placeholder="Họ tên" />
 							</div>
 						</div>
 						<div class="row">
@@ -29,10 +29,10 @@ get_header();
 							  <label for="txt-address" class="inline">Địa chỉ</label>
 							</div>
 							<div class="small-4 columns">
-							  <input type="text" id="txt-address" placeholder="Thành phố" />
+							  <input type="text" name="txt-address" id="txt-address" placeholder="Tỉnh (Thành phố)" />
 							</div>
 							<div class="small-5 columns">
-							  <input type="text" placeholder="Quận (Huyện)" />
+							  <input name="txt-town" type="text" placeholder="Quận (Huyện)" />
 							</div>
 						</div>
 						<div class="row">
@@ -40,7 +40,39 @@ get_header();
 							  <label for="txt-phone" class="inline">Điện thoại</label>
 							</div>
 							<div class="small-9 columns">
-							  <input type="text" id="txt-phone" placeholder="Số điện thoại liên lạc" />
+							  <input type="text" name="txt-phone" id="txt-phone" placeholder="Số điện thoại liên lạc" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="small-3 columns">
+							  <label for="txt-mail" class="inline">Email</label>
+							</div>
+							<div class="small-9 columns">
+							  <input type="text" name="txt-email" id="txt-mail" placeholder="Email liên lạc" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="small-3 columns">
+							  <label for="txt-job" class="inline">Nghề nghiệp</label>
+							</div>
+							<div class="small-9 columns">
+							  <input type="text" name="txt-job" id="txt-job" placeholder="Nghề nghiệp" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="small-3 columns">
+							  <label for="txt-income" class="inline">Thu nhập</label>
+							</div>
+							<div class="small-9 columns">
+							  <input type="text" name="txt-income" id="txt-income" placeholder="Thu nhập hàng tháng" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="small-3 columns">
+							  <label for="txt-issue" class="inline">Lý do nhận nuôi</label>
+							</div>
+							<div class="small-9 columns">
+								<textarea name="txt-issue" id="txt-issue" placeholder="Lý do nhận con nuôi" rows="12"></textarea>
 							</div>
 						</div>
 
@@ -52,10 +84,13 @@ get_header();
 								<label class="inline">Năm sinh</label>
 							  </div>
 							  <div class="large-3 columns">
-								<select>
-									<option value="2013">2013</option>
-									<option value="2013">2013</option>
-									<option value="2013">2013</option>
+								<select name = "txt-birthday">
+									<?php 
+										$tmpyear = date("Y");
+										for($i=$tmpyear;$i>($tmpyear-12);$i--):
+									?>
+									<option value="<?php echo $i ?>"><?php echo $i ?></option>
+									<?php endfor; ?>
 								</select>
 							  </div>
 							  <div class="large-2 columns">
@@ -63,10 +98,10 @@ get_header();
 							  </div>
 							  <div class="large-5 columns">
 								  <div class="large-6 columns">
-									<label class="inline"><input type="radio" style="display:none;" name="txt-gender" /> Nam</label>
+									<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-gender" id="gender1" value="1" checked="checked" /> Nam</label>
 								  </div>
 								  <div class="large-6 columns">
-									<label class="inline"><input type="radio" style="display:none;" name="txt-gender" /> Nữ</label>
+									<label for="gender2" class="inline"><input type="radio" style="display:none;" name="txt-gender" id="gender2" value="2" /> Nữ</label>
 								  </div>
 							  </div>
 							</div>
@@ -74,10 +109,15 @@ get_header();
 							<div class="row">
 							  <div class="large-12 columns">
 								<label>Ghi chú</label>
-								<textarea placeholder="Ghi chú"></textarea>
+								<textarea name="txt-note" placeholder="Ghi chú"></textarea>
 							  </div>
 							</div>
 						  
+						</div>
+						<div class="row">
+							<div class="large-12 columns text-center">
+								<button>Đăng ký</button>
+							</div>
 						</div>
 
 					</fieldset>

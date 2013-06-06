8a3a366f844dc28e0c49095ed11f4b27ec091954
nguyen trong long <traitim9x@gmail.com>
2013-06-06 17:40:23 +0700

    create post nhan con nuoi

--- wp-content/themes/orphans-fd-theme/templates/page-becomeparents.php
+++ wp-content/themes/orphans-fd-theme/templates/page-becomeparents.php
@@ -5,7 +5,43 @@
  * Description: A Page Template that shows form register
  * @author Long Nguyen
  */
- global $current_user;
+ global $current_user, $orphan_prefix;
+ //$data = orphan_fields_ncn_array();
+ if(isset($_POST) && ($_POST != null) && ($_POST['txt-name'] != null)){
+    $hoten = $_POST['txt-name']; //ten
+	$ngaysinh = $_POST['txt-birthday']; // nam sinh
+	$lydo = $_POST['txt-issue'];
+	$gioitinh = $_POST['txt-gender'];
+	$ghichu = $_POST['txt-note'];
+	$thoigianmail = $_POST['txt-month-email'];
+	
+    /*$_POST['txt-address']; //dia chi
+    $_POST['txt-district']  // quan(huyen)
+    $_POST['txt-province'] //tinh
+    $_POST['txt-phone'] // dien thoai
+    $_POST['txt-phone-static'] // dt co dinh
+    $_POST['txt-job'] // nghe nghiep
+    $_POST['txt-income']  // thu nhap
+    //$_POST['txt-issue'] // lý do
+    //$_POST['txt-month-email'] // thoi gian nhan mail
+    
+    //$_POST['txt-gender'] // gioi tinh
+    //$_POST['txt-note'] // ghi chu*/
+	$my_post = array(
+	  'post_title'    => $hoten,
+	  'post_author'   => $current_user->ID,
+	  'post_type'     => 'nhan-con-nuoi'
+	);
+
+	// Insert the post into the database
+	$post_id = wp_insert_post( $my_post );
+	update_post_meta($post_id, $orphan_prefix .'ncn-ly-do', $lydo);
+	update_post_meta($post_id, $orphan_prefix .'ncn-ngay-sinh', $ngaysinh);
+	update_post_meta($post_id, $orphan_prefix .'ncn-gioi-tinh', $gioitinh);
+	update_post_meta($post_id, $orphan_prefix .'ncn-ghi-chu', $ghichu);
+	update_post_meta($post_id, $orphan_prefix .'ncn-month-email', $thoigianmail);
+	wp_redirect(home_url("dang-ky-nhan-mail-thanh-cong"));
+}
 get_header();
 ?>
 
@@ -15,7 +51,7 @@ get_header();
 			<div class="panel" style ="margin-top: 10px;">
 				<h3><span style="font-size:0.8em;">Đón nhận yêu thương</span></h3>
 				<hr />
-				<form class="custom">
+				<form class="custom" method="post">
 					<fieldset>
 						<div class="row">
 							<div class="small-3 columns">
@@ -105,13 +141,13 @@ get_header();
 							  <label class="inline">Nhận email</label>
 							</div>
 							<div class="small-3 columns">
-								<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-month-email" value="1" checked="checked" /> 1 Tháng</label>
+								<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-month-email" value="1 Tháng" checked="checked" /> 1 Tháng</label>
 							</div>
 							<div class="small-3 columns">
-								<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-month-email" value="2" /> 3 Tháng</label>
+								<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-month-email" value="3 Tháng" /> 3 Tháng</label>
 							</div>
 							<div class="small-3 columns">
-								<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-month-email" value="3" /> 6 Tháng</label>
+								<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-month-email" value="6 Tháng" /> 6 Tháng</label>
 							</div>
 						</div>
 
@@ -137,10 +173,10 @@ get_header();
 							  </div>
 							  <div class="large-5 columns">
 								  <div class="large-6 columns">
-									<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-gender" id="gender1" value="1" checked="checked" /> Nam</label>
+									<label for="gender1" class="inline"><input type="radio" style="display:none;" name="txt-gender" id="gender1" value="Nam" checked="checked" /> Nam</label>
 								  </div>
 								  <div class="large-6 columns">
-									<label for="gender2" class="inline"><input type="radio" style="display:none;" name="txt-gender" id="gender2" value="2" /> Nữ</label>
+									<label for="gender2" class="inline"><input type="radio" style="display:none;" name="txt-gender" id="gender2" value="Nữ" /> Nữ</label>
 								  </div>
 							  </div>
 							</div>

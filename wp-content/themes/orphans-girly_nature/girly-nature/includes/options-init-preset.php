c49d368af7d2ded1e33765763215a641ebbd0d54
Ha Nguyen <hant1011@gmail.com>
2013-06-07 02:31:59 +0700

    New

---
+++ wp-content/themes/orphans-girly_nature/girly-nature/includes/options-init-preset.php
@@ -0,0 +1,16 @@
+<?php
+//rename this file to options-init.php to reset the below values, not suggest for normal users.
+
+update_option('gallery-width',558); 	//Gallery Width in Pixels: 545
+update_option('gallery-height',225);	//Gallery Height in Pixels: 225
+update_option('gallery-info',50);		//Text Overlay Height in Pixels: 50
+
+update_option('gallery-border-color',"#000000");	//Gallery Border Color: #546453
+update_option('gallery-bg-color',"#4C4841");			//Gallery Background Color: #F4F4F4
+update_option('gallery-text-color',"#000000");		//Gallery Text Color: #000000
+
+update_option('gallery-delay',50000);							//Slide Display Duration (milliseconds): 50000
+update_option('gallery-fade-duration',1000);					//Slide Fade Duration (milliseconds): 1000
+update_option('gallery-fcg-button','Featured Content');	//Carousel Button Name: Featured Content
+update_option('gallery-rss-word-quantity',45);				//Number of Words in Text Overlay: 45
+?>

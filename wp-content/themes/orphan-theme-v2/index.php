fc98227b0ba336de9593254f021a5f0997b63f18
Ho Nhu Phi <phi.ho@evizi.com>
2013-06-11 17:18:41 +0700

    update news module

--- wp-content/themes/orphan-theme-v2/index.php
+++ wp-content/themes/orphan-theme-v2/index.php
@@ -22,44 +22,50 @@
 				<div class="box-content">
 					<div class="row">
 						<div class="large-7 columns">
+						<?php 
+						$i=0;
+						$featuredPost = new WP_Query();
+						$featuredPost->query('showposts=10&orderby=DESC');
+						if (have_posts()) : while ($featuredPost->have_posts()) : $featuredPost->the_post(); update_post_caches($posts); 		
+						$image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail');
+						if($i < 3){?>
 							<div class="row">
 								<div class="large-4 columns">
-									<a href=""><img alt="title" class="thumb" src="<?php echo get_template_directory_uri(); ?>/images/temp-img/1.png"/></a>
+									<a href="<?php the_permalink(); ?>"><?php if($image[0] != ''){?>
+										<img class="thumb" src="<?php echo $image[0]; ?>" alt="<?php the_title(); ?>" width="226" height="93">
+										<?php }
+										else{?>
+										<img class="thumb" src="<?php echo get_template_directory_uri() ?>/images/no_image.png" width="226" height="93" alt="">
+										<?php }?>
+									</a>
 								</div>
 								<div class="large-8 columns">
-									<h3><a href="">Mỗi bàn tay, một tấm lòng</a></h3>
-									<p> Lorem ipsum dolor sit amet, consectetueadipi scing elit...</p>
+									<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+									<p> <?php the_content_rss('',true,'', 10); ?></p>
 								</div>
 							</div>
-							<div class="row">
-								<div class="large-4 columns">
-									<a href=""><img alt="title" class="thumb" src="<?php echo get_template_directory_uri(); ?>/images/temp-img/2.png"/></a>
-								</div>
-								<div class="large-8 columns">
-									<h3><a href="">Mỗi bàn tay, một tấm lòng</a></h3>
-									<p> Lorem ipsum dolor sit amet, consectetueadipi scing elit...</p>
-								</div>
-							</div>
-							<div class="row">
-								<div class="large-4 columns">
-									<a href=""><img alt="title" class="thumb" src="<?php echo get_template_directory_uri(); ?>/images/temp-img/4.png"/></a>
-								</div>
-								<div class="large-8 columns">
-									<h3><a href="">Mỗi bàn tay, một tấm lòng</a></h3>
-									<p> Lorem ipsum dolor sit amet, consectetueadipi scing elit...</p>
-								</div>
-							</div>
+						<?php }
+						else {
+						if($i == 3){ ?>
 						</div>
 						<div class="large-5 columns meta-list">
 							<ul class="meta">
-								<li><a href="#">Những vòng tay yêu thương</a><span class="date"> (12/06/2013)</span></li>
-								<li><a href="#">Thắp sáng ước mơ bằng những yêu thương</a><span class="date">(12/06/2013)</span></li>	
-								<li><a href="#">Khi giắc mơ về bên nụ cười trẻ thơ </a><span class="date">(12/06/2013)</span></li>
-								<li><a href="#">6 bước biến ước mơ thành hiện thực</a><span class="date">(12/06/2013)</span></li>
-								<li><a href="#">Thắp sáng ước mơ bằng những yêu thương </a><span class="date">(12/06/2013)</span></li>
+								<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								<?php $my_date = the_date('d-m-Y', '<span class="date">', '</span>', FALSE); if($my_date != '') echo '('.$my_date.')' ?></li>
+						<?php }
+						else{ ?>
+								<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								<?php $my_date = the_date('d-m-Y', '<span class="date">', '</span>', FALSE); if($my_date != '') echo '('.$my_date.')' ?></li>
+						<?php }
+						}
+						?>
 							
+						<?php $i++; endwhile;  wp_reset_query(); ?>
 							</ul>
 						</div>
+						<?php endif; ?>
+							
+						
 					</div>
 					
 				</div>

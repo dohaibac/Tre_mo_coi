091c305885439eb3858e02d1afe143ebb767af0f
Ho Nhu Phi <phi.ho@evizi.com>
2013-06-26 14:22:59 +0700

    commit tre-mo-coi pages

--- wp-content/themes/orphan-theme-v2/loop-tre-mo-coi.php
+++ wp-content/themes/orphan-theme-v2/loop-tre-mo-coi.php
@@ -2,17 +2,26 @@
 if (have_posts()) :
     while (have_posts()) : the_post();
         update_post_caches($posts);
-        $image = orphan_get_post_thumbnai(); // wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail');
+        $image = get_post_meta( get_the_ID(), 'orphan_photo', true );//orphan_get_post_thumbnai(); // wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail');
+		if($image == '') $image = get_template_directory_uri().'/images/no_image.png' ;
+		$birdthday = get_post_meta( get_the_ID(), 'orphan_birthday', true );
+		$gender = get_post_meta( get_the_ID(), 'orphan_gender', true );
+		$address = get_post_meta( get_the_ID(), 'orphan_cv-address', true );
+		$time_meet = get_post_meta( get_the_ID(), 'orphan_cv-time', true );
+		//$content = get_post_meta( get_the_ID(), 'orphan_cv-content', true );
         ?>
         <div class="row">
-            <div class="large-3 columns">
+            <div class="large-2 columns">
                 <a rel="tooltip" href="<?php the_permalink(); ?>" class="btn">
                     <img class="thumb" src="<?php echo $image; ?>" alt="<?php the_title(); ?>" width="226" height="93">
                 </a>
             </div>
-            <div class="large-9 columns">
+            <div class="large-10 columns">
                 <h3><a rel="tooltip" href="<?php the_permalink(); ?>" class="btn"><?php the_title(); ?></a></h3>
-                <p><?php the_content_rss('', true, '', 30); ?></p>
+                <p>Ngày sinh: <?php echo $birdthday ?></p>
+				<p>Giới : <?php echo $gender ?></p>
+				<p>Địa điểm gặp trẻ: <?php echo $address ?></p>
+				<p>Thời gian gặp trẻ: <?php echo $time_meet ?></p>
             </div>
         </div>
     <?php endwhile; ?>

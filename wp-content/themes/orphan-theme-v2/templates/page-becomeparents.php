224d283a25e31f71a81d97d7c88c276e66ab9cfd
hant1011 <ha.nguyen@evizi.com>
2013-06-28 10:09:48 +0700

    becomeparent

--- wp-content/themes/orphan-theme-v2/templates/page-becomeparents.php
+++ wp-content/themes/orphan-theme-v2/templates/page-becomeparents.php
@@ -50,11 +50,13 @@ function add_javascript()
 {
 	global $THEME_DIR_URL;
     $action_ajax_url = admin_url('admin-ajax.php');
+    $site_url = get_site_url();
     
     echo <<<EOF
 	<script type="text/javascript" src="{$THEME_DIR_URL}/js/tre-mo-coi/page-becomeparents.js"></script>
 	<script>
-		var action_ajax_url ='$action_ajax_url';</script>";
+		var action_ajax_url ='$action_ajax_url';
+		var site_url ='$site_url';
 	</script>
 EOF;
 }
@@ -81,6 +83,40 @@ get_header();
 						
 						<div data-alert="" class="alert-box alert" style="display:none;margin-bottom:5px;" id="becomeparents_success_container"></div>
 						<div data-alert="" class="alert-box alert" style="display:none;margin-bottom:5px;" id="becomeparents_error_container"></div>
+						<div id="becomeparents_success" style="display:none;">
+							<div style="width: 600px; height: auto;" id="register">
+								<div>
+									<img width="600" src="<?php echo $THEME_DIR_URL?>/images/alert-lightbox-header.png">
+								</div>
+								<br>
+								<div style="text-align:left;margin-top:20px;margin-bottom:30px;line-height:25px;">
+									<div class="row">
+										<div class="large-12 columns"><span style="font-weight:200;font-size:25px;">Chúc mừng bạn đã đăng ký nhận thông tin trẻ thành công!</span></div>
+									</div>
+									<br />
+									<div class="row">
+										<div class="large-12 columns">
+											Chúng tôi đã sẽ gửi thông tin những trẻ phù hợp với nhu cầu của ban đến <br />địa chỉ <span id="email-and-duration"></span>.
+										</div>
+									</div>
+									<br />
+									<div class="row">
+										<div class="large-12 columns">
+											 <i><u>Ghi chú</u>: Bạn có thể hủy chức năng nhận email theo đường dẫn tại email mà bạn nhận được.</i>
+										</div>
+									</div>
+									<br />
+									<div class="row" style="text-align:center">
+										<div class="large-6 columns">
+											 <a id="back_to_home" >Trở lại trang chủ</a>
+										</div>
+										<div class="large-6 columns">
+											 <a id="back_to_previous_page">Trở lại trang trước đó</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 						
 						<form id="becomeparents_form" class="custom" method="post">
 							<input type="hidden" name="become_parent" value="become_parent" />
@@ -134,10 +170,10 @@ get_header();
 									  &nbsp;
 									</div>
 									<div class="small-4 columns">
-									  <input type="text" name="txt_district" placeholder="Quận (Huyện)" value="<?php echo get_user_meta($current_user->ID, "district", true); ?>" />
+									  <input type="text" name="txt_district" id="txt_district" placeholder="Quận (Huyện)" value="<?php echo get_user_meta($current_user->ID, "district", true); ?>" />
 									</div>
 									<div class="small-5 columns">
-									  <input name="txt_province" type="text" placeholder="Tỉnh (Thành phố)" value="<?php echo get_user_meta($current_user->ID, "province", true); ?>" />
+									  <input name="txt_province" id="txt_province" type="text" placeholder="Tỉnh (Thành phố)" value="<?php echo get_user_meta($current_user->ID, "province", true); ?>" />
 									</div>
 								</div>
 								<div class="row">
@@ -153,7 +189,7 @@ get_header();
 									  <label for="txt_phone_static" class="inline">Điện thoại cố định</label>
 									</div>
 									<div class="small-9 columns">
-									  <input type="text" name="txt_phone_static" placeholder="Số điện thoại cố định (tùy chọn)" value="<?php echo get_user_meta($current_user->ID, "phone_static", true); ?>" />
+									  <input type="text" name="txt_phone_static" id="txt_phone_static" placeholder="Số điện thoại cố định (tùy chọn)" value="<?php echo get_user_meta($current_user->ID, "phone_static", true); ?>" />
 									</div>
 								</div>
 								<div class="row">
@@ -212,7 +248,7 @@ get_header();
 										<label class="inline">Năm sinh</label>
 									  </div>
 									  <div class="large-3 columns" align="left">
-										<select name = "txt_birthyear">
+										<select name = "sel_birthyear" id="sel_birthyear">
 											<?php 
 												$tmpyear = date("Y");
 												for($i=$tmpyear;$i>($tmpyear-12);$i--):
@@ -244,7 +280,7 @@ get_header();
 											<label>Ghi chú</label>
 										</div>
 										<div class="large-9 columns">
-											<textarea name="txt_note" placeholder="Ghi chú"></textarea>
+											<textarea name="txt_note" id="txt_note" placeholder="Ghi chú"></textarea>
 										</div>
 									</div>
 									

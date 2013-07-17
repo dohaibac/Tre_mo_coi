94a13b54942d972ba53449cb5547dc34be1d84f7
nguyen trong long <traitim9x@gmail.com>
2013-07-17 14:49:57 +0700

    update upload file excel

--- wp-content/plugins/insert-excel-tmc/insert-excel-tmc.php
+++ wp-content/plugins/insert-excel-tmc/insert-excel-tmc.php
@@ -15,4 +15,8 @@ function import_excel_page_template( $page_template )
     }
     return $page_template;
 }
+add_action('insert_excel_link','tmc_insert_excel_mt');
+function tmc_insert_excel_mt(){
+	echo '<br/><p style="color:red;font-size:0.9em;padding-left:10px;"><strong>Chú ý: Bạn có thể thêm nhiều trẻ theo danh sách từ file Excel <a href="'.home_url("nhap-danh-sach-tre").'">tại đây</a></strong></p>';
+}
 ?>
